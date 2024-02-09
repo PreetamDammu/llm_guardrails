b0a8e2f1911e466cc0eabe4cbb8060da6cd68851
Preetam Dammu <preetams@uw.edu>
2024-02-09 22:20:01 +0000

    Tidy file

--- utils/openai_helpers.py
+++ utils/openai_helpers.py
@@ -23,33 +23,3 @@ def query_openai_model(model_name, prompt):
     )
 
     return response.choices[0].message.content
-
-# import openai
-# import time
-# from dotenv import dotenv_values
-
-# secrets = dotenv_values(".env")
-# personal_api_key = secrets['OPENAI_API_KEY']
-
-# from openai import OpenAI
-
-# client = OpenAI(
-#     # defaults to os.environ.get("OPENAI_API_KEY")
-#     api_key=personal_api_key,
-# )
-
-# def query_chat_gpt(prompt, model_name, num_retries=10):
-#     for _ in range(num_retries):
-#         try:
-#             response = client.chat.completions.create(
-#                 model= model_name, 
-#                 messages=[{"role": "user", "content": prompt}]
-#             )
-#             return response.choices[0].message.content.strip()
-#         except Exception as e:
-#             print(e)
-#             print("Retrying...")
-#             time.sleep(10)
-#             continue
-#     print(f"Failed after {num_retries} retries")
-#     return None
